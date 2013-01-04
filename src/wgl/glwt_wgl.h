48052e854ef7fadc3fd21a2a6e05ed3cf04feb02
Riku Salminen <riku.salminen@tkk.fi>
2013-01-04 14:03:30 +0200

    Win32 basic initialization
    Initialization of Win32 with WGL and EGL.

--- src/wgl/glwt_wgl.h
+++ src/wgl/glwt_wgl.h
@@ -1,8 +1,21 @@
 #ifndef GLWT_glwt_wgl_h
 #define GLWT_glwt_wgl_h
 
+#include <GLXW/glxw_wgl.h>
+
 struct glwt_wgl
 {
 };
+
+struct glwt_window_wgl
+{
+    HGLRC context;
+};
+
+int glwtInitWGL(const GLWTConfig *config);
+void glwtQuitWGL();
+
+int glwtWindowCreateWGL(GLWTWindow *win, GLWTWindow *share);
+void glwtWindowDestroyWGL(GLWTWindow *window);
 
 #endif
